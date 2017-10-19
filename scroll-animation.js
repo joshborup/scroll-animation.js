746f718afe9ef7193ed39a084d906e42185336a0
Joshua D Borup <joshborup@gmail.com>
2017-10-19 09:54:08 -0700

    added main js file

---
+++ scroll-animation.js
@@ -0,0 +1,40 @@
+var $animation_elements = $('.animation-element');
+var $window = $(window);
+
+function check_if_in_view() {
+  var window_height = $window.height();
+  var window_top_position = $window.scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+ 
+  $.each($animation_elements, function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+    var trigger_position = (element_top_position  <= window_bottom_position - 150);
+ 
+    //check to see if this current container is within viewport
+    if (trigger_position && ($element.hasClass('slide-up') == true)) {
+       
+            
+            $element.addClass('in-view-up');
+            
+           
+    } else if (trigger_position && ($element.hasClass('slide-left') == true)) {
+       
+            
+            $element.addClass('in-view-left');
+            
+           
+    } else if (trigger_position && ($element.hasClass('slide-right') == true)) {
+       
+            
+            $element.addClass('in-view-right');
+            
+           
+    }
+      
+  });
+}
+
+$(window).on('scroll', check_if_in_view);
