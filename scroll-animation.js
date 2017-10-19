a44d0e99a78d027a29aa55b786a93e0e723587be
Joshua D Borup <joshborup@gmail.com>
2017-10-19 16:16:39 -0700

    Added Throttle to Scroll event for performance

--- scroll-animation.js
+++ scroll-animation.js
@@ -39,4 +39,15 @@ function check_if_in_view() {
         
   });
 }
-$(window).on('scroll', check_if_in_view);
+
+function throttle(fn, wait) {
+  var time = Date.now();
+  return function() {
+    if ((time + wait - Date.now()) < 0) {
+      fn();
+      time = Date.now();
+    }
+  }
+}
+
+$(window).on('scroll', throttle(check_if_in_view, 200));
